85f000cc6c7cd33caf8551084af102eed2611f67
Lionel Reveillere <lionelreveillere2@hotmail.fr>
2011-11-06 15:53:09 +0100

    Chordable + Chord

--- src/chord/Chord.java
+++ src/chord/Chord.java
@@ -1,5 +1,21 @@
 package chord;
 
-public class Chord {
+public class Chord implements Chordable {
+
+	@Override
+	// ask node n to find the successor of id
+	public int find_successor(int id) {
+		// TODO Auto-generated method stub
+		/*
+	    if (id  (n, successor] ) // Yes, that should be a closing square bracket to match the opening parenthesis. It is a half closed interval.
+			return successor;
+	    else
+	     // forward the query around the circle
+	     n0 = closest_preceding_node(id);
+	     return n0.find_successor(id);
+		*/
+		return 0;
+	}
+
 
 }
---
+++ src/chord/Chordable.java
@@ -0,0 +1,8 @@
+package chord;
+
+public interface Chordable {
+
+	// ask node n to find the successor of id
+	public int find_successor(int id);
+	
+}
